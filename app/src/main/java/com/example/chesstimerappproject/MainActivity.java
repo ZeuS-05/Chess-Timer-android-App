02761687911b303dfd45a24640467347b526beef
Ujjayraj <65146978+ZeuS-05@users.noreply.github.com>
2024-05-28 19:39:07 +0800

    working 1

--- app/src/main/java/com/example/chesstimerappproject/MainActivity.java
+++ app/src/main/java/com/example/chesstimerappproject/MainActivity.java
@@ -1,151 +1,78 @@
+// MainActivity.java
 package com.example.chesstimerappproject;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
-
-    package com.example.chesstimer;
-
-import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-    public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-        private TextView player1TimerTextView;
-        private TextView player2TimerTextView;
-        private Button startStopPlayer1Button;
-        private Button startStopPlayer2Button;
+    private TextView timer1, timer2;
+    private Button startButton, pauseButton, resetButton;
 
-        private CountDownTimer player1CountDownTimer;
-        private CountDownTimer player2CountDownTimer;
+    private Handler handler = new Handler();
+    private boolean isRunning = false;
+    private long startTime = 0;
 
-        private long player1TimeLeftInMillis = 600000; // 10 minutes in milliseconds
-        private long player2TimeLeftInMillis = 600000; // 10 minutes in milliseconds
-
-        private boolean player1TimerRunning = false;
-        private boolean player2TimerRunning = false;
-
+    private Runnable timerRunnable = new Runnable() {
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
 
-            player1TimerTextView = findViewById(R.id.player1_timer);
-            player2TimerTextView = findViewById(R.id.player2_timer);
-            startStopPlayer1Button = findViewById(R.id.start_stop_player1);
-            startStopPlayer2Button = findViewById(R.id.start_stop_player2);
+            timer1.setText(String.format("%02d:%02d", minutes, seconds));
+            timer2.setText(String.format("%02d:%02d", minutes, seconds));
 
-            startStopPlayer1Button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (player1TimerRunning) {
-                        stopPlayer1Timer();
-                    } else {
-                        startPlayer1Timer();
-                    }
-                }
-            });
-
-            startStopPlayer2Button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (player2TimerRunning) {
-                        stopPlayer2Timer();
-                    } else {
-                        startPlayer2Timer();
-                    }
-                }
-            });
-
-            updateTimerText();
+            handler.postDelayed(this, 500);
         }
-
-        private void startPlayer1Timer() {
-            player1CountDownTimer = new CountDownTimer(player1TimeLeftInMillis, 1000) {
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    player1TimeLeftInMillis = millisUntilFinished;
-                    updateTimerText();
-                }
-
-                @Override
-                public void onFinish() {
-                    player1TimerRunning = false;
-                    startStopPlayer1Button.setText("Start Player 1");
-                }
-            }.start();
-
-            player1TimerRunning = true;
-            startStopPlayer1Button.setText("Pause Player 1");
-        }
-
-        private void stopPlayer1Timer() {
-            player1CountDownTimer.cancel();
-            player1TimerRunning = false;
-            startStopPlayer1Button.setText("Start Player 1");
-        }
-
-        private void startPlayer2Timer() {
-            player2CountDownTimer = new CountDownTimer(player2TimeLeftInMillis, 1000) {
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    player2TimeLeftInMillis = millisUntilFinished;
-                    updateTimerText();
-                }
-
-                @Override
-                public void onFinish() {
-                    player2TimerRunning = false;
-                    startStopPlayer2Button.setText("Start Player 2");
-                }
-            }.start();
-
-            player2TimerRunning = true;
-            startStopPlayer2Button.setText("Pause Player 2");
-        }
-
-        private void stopPlayer2Timer() {
-            player2CountDownTimer.cancel();
-            player2TimerRunning = false;
-            startStopPlayer2Button.setText("Start Player 2");
-        }
-
-        private void updateTimerText() {
-            int minutes1 = (int) (player1TimeLeftInMillis / 1000) / 60;
-            int seconds1 = (int) (player1TimeLeftInMillis / 1000) % 60;
-
-            int minutes2 = (int) (player2TimeLeftInMillis / 1000) / 60;
-            int seconds2 = (int) (player2TimeLeftInMillis / 1000) % 60;
-
-            String timeLeftFormatted1 = String.format("%02d:%02d", minutes1, seconds1);
-            String timeLeftFormatted2 = String.format("%02d:%02d", minutes2, seconds2);
-
-            player1TimerTextView.setText(timeLeftFormatted1);
-            player2TimerTextView.setText(timeLeftFormatted2);
-        }
-    }
-
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        timer1 = findViewById(R.id.timer1);
+        timer2 = findViewById(R.id.timer2);
+        startButton = findViewById(R.id.startButton);
+        pauseButton = findViewById(R.id.pauseButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isRunning) {
+                    startTime = System.currentTimeMillis();
+                    handler.post(timerRunnable);
+                    isRunning = true;
+                }
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isRunning) {
+                    handler.removeCallbacks(timerRunnable);
+                    isRunning = false;
+                }
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.removeCallbacks(timerRunnable);
+                isRunning = false;
+                timer1.setText("00:00");
+                timer2.setText("00:00");
+            }
         });
     }
 }
